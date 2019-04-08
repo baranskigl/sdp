3b28c9d27d857b58446516f8e0e3c0d3b48d2563
Bartosz Barański <bartosz.marek.baranski@gmail.com>
2019-04-08 18:03:46 +0200

    SDP 2019 Java preparation

---
+++ interviews/src/main/java/com/globallogic/sdp/interviews/Sdp.java
@@ -0,0 +1,25 @@
+package com.globallogic.sdp.interviews;
+
+public class Sdp {
+
+    /**
+     * Co to jest dziedziczenie?
+     * Co to jest Polimorfizm?
+     * Czy w jawie występuje dzidziczenie wielokrotne
+     * Jakiego słowa kluczowego należy użyć żeby rozszerzyć klasę?
+     * Co to jest interfejs?
+     * Co to jest klasa abstrakcyjna?
+     * Typy Danych, do czego służą?
+     * Co to jest wyjątek?
+     * Co to są kolekcje?
+     * W jakiej sytuacji może się pojawić wyjątek NullPointerException?
+     * W jaki sposób stworzyć własny wyjątek?
+     * Czym się różni w kodzie klasy, konstruktor od metody?
+     * Co to jest Garbage Collection (GC)?
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
